cf9006c220463e12c70e6f88d0755bd305b53a66
Guido Stocco <guidostoccodev@gmail.com>
2024-06-09 14:14:25 -0300

    seleção de elementos

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 // Seleção de elementos
 const multiplicationForm = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicatorInput = document.querySelector("#multiplicator");
+
+
+
+// funções
+
+
+
+
+// eventos
